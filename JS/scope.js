9ceb03bc20d954c2bbae47c9a5d6335895e2451b
kathyaalejandra <kathyaalejandra.mansilla@alumnos.ulagos.cl>
2024-09-04 19:24:42 -0400

    agregué un nuevo archivo

---
+++ JS/scope.js
@@ -0,0 +1,10 @@
+function teamcap () {
+    var heroe = "Capitan America";
+    console.log(heroe);
+    //Capitan America
+}
+function teamironman () {
+    var heroe = "Iron Man";
+    console.log(heroe);
+    //Iron Man
+}
