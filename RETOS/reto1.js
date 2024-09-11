d3644c402921f22831870fd2ef837b6fe8ceefdd
kathyaalejandra <kathyaalejandra.mansilla@alumnos.ulagos.cl>
2024-09-11 20:13:23 -0300

    Código comentado

--- RETOS/reto1.js
+++ RETOS/reto1.js
@@ -1,11 +1,15 @@
+//Pidiendo al usuario ingresar una oración
 let cadena = prompt('Ingrese una cadena de texto', '');
-cadena = cadena.toLocaleLowerCase();
-let contadorA = 0;
+cadena = cadena.toLocaleLowerCase(); //Poner todos los caracteres en minúscula 
+//Comenzando los contadores de vocales desde 0
+
+let contadorA = 0;                   
 let contadorE = 0;
 let contadorI = 0;
 let contadorO = 0;
 let contadorU = 0;
 
+//Comienza el proceso para contar y sumar las vocales
 for (let i = 0; i < cadena.length; i++){
     let caracter = cadena[i];
 
@@ -21,7 +25,7 @@ for (let i = 0; i < cadena.length; i++){
         contadorU++;
     }
 }
-
+//Mostrando las cantidades de cada vocal
 console.log('Número de veces de cada vocal: ');
 console.log(`Cantidad de a: ${contadorA}`);
 console.log(`Cantidad de e: ${contadorE}`);
