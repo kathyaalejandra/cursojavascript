30234b14c1e388fafaa9337fd172c96e1319665d
kathyaalejandra <kathyaalejandra.mansilla@alumnos.ulagos.cl>
2024-09-24 15:48:33 -0300

    probando diferentes funciones

---
+++ JS/funciones.js
@@ -0,0 +1,15 @@
+function teamcap() {
+    var heroe = "Capitán América";
+    console.log(heroe);
+}
+function teamIronMan() {
+    var heroe = "Iron Man";
+    console.log(heroe);
+}
+function ligajusticia() {
+    var heroes = ['Batman', 'Superman', 'Aquaman']
+    console.log(heroes)
+}
+teamcap();
+teamIronMan();
+ligajusticia();
