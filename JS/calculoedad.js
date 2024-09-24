8aa5f73ef90ec402b1522ca6ac9c44b554db9637
kathyaalejandra <kathyaalejandra.mansilla@alumnos.ulagos.cl>
2024-09-24 15:11:04 -0300

    otros tipos de ejercicios

---
+++ JS/calculoedad.js
@@ -0,0 +1,7 @@
+//Funcion para calcular la edad del usuario
+const anonacimiento = prompt("Ingrese su año de nacimiento");
+const anoactual = 2024;
+
+let edad = anoactual - anonacimiento;
+
+alert(`Tienes ${edad} años!`);
