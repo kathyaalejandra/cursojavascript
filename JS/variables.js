628188db6f116021bad881a6e3893ef8ad9f4cdc
kathyaalejandra <kathyaalejandra.mansilla@alumnos.ulagos.cl>
2024-09-24 00:43:22 -0300

    practicando

---
+++ JS/variables.js
@@ -0,0 +1,24 @@
+//Variables que cambian con el tiempo
+var altura = 1.70;
+let edad = 23;
+
+//Variable que no cambia con el tiempo
+const nombre = "Kathya";
+const apellido = "Mansilla";
+
+// OTROS EJEMPLOS
+// Declarando las Variables
+var peso
+let ciudad
+
+// Inicializando las Variables anteriores
+edad = 60
+ciudad = "Ancud"
+
+// Concatenando Variables de tipo String
+const nombrecompleto = nombre + " " + apellido //Las comillas hacen un espacio entre las variables
+console.log("Mi nombre es Kathya Mansilla");
+console.log('Mi nombre es Kathya Mansilla');
+console.log(nombrecompleto);
+console.log(`Mi nombre es ${nombre} ${apellido}`);
+console.log(`Mi nombre es: "${nombrecompleto}"`);
