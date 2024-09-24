ea657217c36fac876bcb3c11df3e9a6a14afd143
kathyaalejandra <kathyaalejandra.mansilla@alumnos.ulagos.cl>
2024-09-24 16:41:10 -0300

    utilizando a++ y a--

---
+++ JS/operadorunario.js
@@ -0,0 +1,12 @@
+//ciclo incremento
+for (let i = 0; i < 5; i++) {
+    console.log(i); // Imprime 0, 1, 2, 3, 4
+  }
+//ciclo decremento
+  for (let i = 5; i > 0; i--) {
+    console.log(i); // Imprime 5, 4, 3, 2, 1
+  }
+//contador
+let contador = 10;
+contador++; // 1
+console.log(contador); // 1
