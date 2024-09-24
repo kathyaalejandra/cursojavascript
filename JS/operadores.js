2a6bc7731d4fda56e5559864cd9095c243c8f576
kathyaalejandra <kathyaalejandra.mansilla@alumnos.ulagos.cl>
2024-09-24 16:26:53 -0300

    probando operadores

---
+++ JS/operadores.js
@@ -0,0 +1,27 @@
+console.log("El primer número es 5 y el segundo es 7");
+
+let num1 = 5;
+let num2 = 7;
+
+let suma = num1 + num2;
+let resta = num1 - num2;
+let multiplicacion = num1 * num2;
+let division = num1 / num2;
+let resto = num1 % num2;
+let potencia = num1 ** num2;
+let sumaasignación = num1 += 10;
+let restaasignación = num1 -= 10;
+let multiasignacion = num1 *= 10;
+let divasignacion = num1 /= num1;
+
+
+console.log(`La suma es ${suma}`);
+console.log(`La resta es ${resta}`);
+console.log(`La multiplicación es ${multiplicacion}`);
+console.log(`La división es ${division}`);
+console.log(`La resto de la división es ${resto}`);
+console.log(`La potencia es ${potencia}`);
+console.log(`La suma y asignación es ${sumaasignación}`);
+console.log(`La resta y asignación es ${restaasignación}`);
+console.log(`La multiplicación y asignación es ${multiasignacion}`);
+console.log(`La división y asignación es ${divasignacion}`);
