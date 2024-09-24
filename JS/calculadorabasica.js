69bbdac938bcd7c8364dba307c7b6df364906ea5
kathyaalejandra <kathyaalejandra.mansilla@alumnos.ulagos.cl>
2024-09-24 15:48:20 -0300

    calculadora básica de números

---
+++ JS/calculadorabasica.js
@@ -0,0 +1,14 @@
+
+//Se utilza parseFloat para reconocer que son números y no cadena de texto
+let primeravariable = parseFloat(prompt ("Ingrese un número: "));
+let segundavariable = parseFloat(prompt("Ingrese otro número:"));
+
+let suma = primeravariable + segundavariable;
+let resta = primeravariable - segundavariable;
+let division = primeravariable / segundavariable;
+let multiplicacion = primeravariable * segundavariable;
+
+console.log (`La Suma de los números es: ${suma}`);
+console.log (`La Resta de los números es: ${resta}`);
+console.log (`La División de los números es: ${division}`);
+console.log (`La Multiplicación de los números es: ${multiplicacion}`);
