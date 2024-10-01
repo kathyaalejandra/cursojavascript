c7ccf19db24e41e4bc711315d69ae9722dbf8d6e
kathyaalejandra <kathyaalejandra.mansilla@alumnos.ulagos.cl>
2024-10-01 19:20:02 -0300

    archivo de como hacer funciones

---
+++ JS/funciones2.js
@@ -0,0 +1,13 @@
+function mostrarMensaje() {
+    console.log('¡Hola a todos!');
+}
+
+function saludar() {
+    return "Hola";
+}
+saludar();
+
+const saludo = function saludar(){
+    return "Hola!";
+}
+saludo();
