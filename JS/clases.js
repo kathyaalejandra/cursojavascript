67da467f417375b143296bfe9e022986444af51a
kathyaalejandra <kathyaalejandra.mansilla@alumnos.ulagos.cl>
2024-10-01 19:28:35 -0300

    archivo de clases

---
+++ JS/clases.js
@@ -0,0 +1,7 @@
+//Molde que permite crear objetos con ciertas caracteristicas en común
+
+//Tenemos una clase vacía
+class Animal {
+//Crear un objeto basada en una clase
+}
+const perro = new Animal();
