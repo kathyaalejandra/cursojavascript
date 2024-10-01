ffdc8d02a1771757aa9d54f8d730615d97ebd861
kathyaalejandra <kathyaalejandra.mansilla@alumnos.ulagos.cl>
2024-10-01 19:28:44 -0300

    agregando más código

--- JS/funciones2.js
+++ JS/funciones2.js
@@ -1,12 +1,17 @@
+//Encapsular metodo y logicas
+//Sirve para reutilizar código, es um tipo de dato más
+
+//Faltaría el mostrarMensaje() para que se vea
 function mostrarMensaje() {
     console.log('¡Hola a todos!');
 }
-
+//Función por declaración
 function saludar() {
     return "Hola";
 }
 saludar();
-
+//Función por expresión
+//Para reinvocar una función
 const saludo = function saludar(){
     return "Hola!";
 }
